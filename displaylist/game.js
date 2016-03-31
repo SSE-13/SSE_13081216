c2673d611332985eb4b61a53a029859646f8fe39
White <White>
2016-03-31 15:27:53 +0800

    修改

--- displaylist/game.js
+++ displaylist/game.js
@@ -3,25 +3,56 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var human = new render.DisplayObjectContainer();
+human.x = -50;
+human.y = -120;
 var humanContainer = new render.DisplayObjectContainer();
 var head = new render.Bitmap();
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
+head.x = 10;
+head.y = -90;
+var trunk = new render.Bitmap();
+trunk.x = 20;
+var left_arm = new render.Bitmap();
+left_arm.x = -10;
+left_arm.y = 30;
+var right_arm = new render.Bitmap();
+right_arm.x = 95;
+right_arm.y = 30;
+var left_leg = new render.Bitmap();
+left_leg.x = 0;
+left_leg.y = 130;
+var right_leg = new render.Bitmap();
+right_leg.x = 60;
+right_leg.y = 120;
+head.source = "head.png";
+trunk.source = "body.png";
+left_arm.source = "L_arm.png";
+right_arm.source = "R_arm.png";
+left_leg.source = "L_leg.png";
+right_leg.source = "R_leg.png";
+humanContainer.addChild(human);
+human.addChild(left_arm);
+human.addChild(right_arm);
+human.addChild(left_leg);
+human.addChild(right_leg);
+human.addChild(head);
+human.addChild(trunk);
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+renderCore.start(humanContainer, ["L_arm.png", "R_arm.png", "L_leg.png", "R_leg.png", "head.png", "body.png"]);
 var HumanBody = (function (_super) {
     __extends(HumanBody, _super);
     function HumanBody() {
         _super.apply(this, arguments);
     }
     HumanBody.prototype.onTicker = function (duringTime) {
-        // this.x = 
-        // this.y = 
-        // this.rotation =
+        this.x += this.vx * duringTime;
+        this.rotation += Math.PI * duringTime;
     };
     return HumanBody;
 }(Body));
 var ticker = new Ticker();
 var body = new HumanBody(humanContainer);
+body.vx = 5;
+body.y = 200;
 ticker.start([body]);
 //# sourceMappingURL=game.js.map
