4698bab8992a596a72de5685532675f7089b5108
SHK <676353856@qq.com>
2016-04-07 16:29:50 +0800

    Finish Homework 6

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -63,32 +63,70 @@ ticker.start([body]);
 var eventCore = new events.EventCore();
 eventCore.init();
 
-var headHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
-    alert (`点击位置为${localPoint.x},${localPoint.y}`);
-    return true;
-}
+var isHead = 0;
+var ClickedHead = false;
+var isLeg = 0;
+var ClickedLeg = false;
 
-var headOnClick = function() {
-     if (isHead == 1) {
-        if (body.vx != 0) {
+var headHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
+   // alert (`点击位置为${localPoint.x},${localPoint.y}`);
+   console.log(localPoint);
+
+    if(localPoint.x <= Math.abs(displayObject.x * 6) && localPoint.y <= Math.abs(displayObject.y)&&
+    localPoint.x > 0 && localPoint.y > 0){
+        isHead += 1;
+        ClickedHead = true;
+}
+    return ClickedHead;
+}
+var LegHitTest = (localPoint:math.Point,displayObject:render.DisplayObject) =>{
+   // alert (`点击位置为${localPoint.x},${localPoint.y}`);
+   console.log(localPoint);
+
+   if(localPoint.x > 0 && localPoint.x <=  Math.abs(displayObject.x * 2) && localPoint.y > 0 && localPoint.y < Math.abs(displayObject.y * 2)){
+        isLeg += 1;
+        ClickedLeg = true;
+}
+    return ClickedLeg;
+}
+var HeadOnClick = () => {
+
+    if(isHead == 1){
+        if(body.vx!=0){
             body.vx *= -1;
             body.vrotation *= -1;
         }
-        if (body.vx == 0) {
+        if(body.vx == 0){
             isHead = 0;
-        }
+        }        
     }
-    if (isHead != 1) {
+
+    if(isHead != 1){
         body.vx = 5;
-        body.vrotation = Math.PI / 2;
+        body.vrotation = Math.PI/2;
         isHead = 0;
     }
     ClickedHead = false;
-    console.log("clickhead:" + isHead);
-    //修改 HumanBody 的速度，使其反向移动
+    console.log("clickhead:"+isHead);
 }
 
-eventCore.register(head,headHitTest,headOnClick);
+var LegOnClick = () => {
+   
+    if(isLeg == 1){
+        body.vx = 0;
+        body.vrotation = 0;
+        body.rotation = 0;
+    }
+    if(isLeg >= 1){       
+        isLeg = 0;
+    }
+    ClickedLeg = false;
+    console.log("clickleg:"+ isLeg);
+}
+
+eventCore.register(head,headHitTest,HeadOnClick);
+eventCore.register(left_leg,headHitTest,LegOnClick);
+eventCore.register(right_leg,headHitTest,LegOnClick);
 
 
 
