a75923268760d01c5b7af3e1827d697e132cdb79
SHK <676353856@qq.com>
2016-04-27 20:50:59 +0800

    save File

--- gameengine/src/data.ts
+++ gameengine/src/data.ts
@@ -22,6 +22,14 @@ module data {
         }
         
         public saveFile(){
+            var json = {"map":[mapData[0], mapData[1], mapData[2], mapData[3],mapData[4], mapData[5], mapData[6], mapData[7]]};
+            var obj = JSON.stringify(json);
+            var map_path = __dirname + "/map.json";
+            fs.writeFile(map_path, obj, function (err) {
+               if (err) throw err;
+               console.log('Save Done!');
+               alert("Save Done!");
+            });
             
         }
         
