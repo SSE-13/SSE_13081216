bd2a55329894f2cc263166d8385a5c68f15f43c9
SHK13081216 <676353856@qq.com>
2016-04-28 02:41:56 +0800

    command

--- gameengine/src/command.ts
+++ gameengine/src/command.ts
@@ -1,94 +1,218 @@
-module command{
+module command {
+
     export class Invoker {
-    
-    private _list:Array<any>;
-    public new_command;
-    
-    init(){
-        this._list = [];
+        private _list: Array<any>;
+
+        init() {
+            this._list = [];
+        }
+
+        canUndo() {
+            return this._list.length > 0;
+        }
+
+        undo() {
+            var command = this._list.pop();
+            return command.undo();
+        }
+        setCommand(command) {
+            command.execute();
+            this._list.push(command);
+            console.log(this._list.length);
+        }
     }
-    
-    public canUndo(){
-        return this._list.length > 0;
+
+    export class Command {
+
+        constructor() {
+
+        }
+        getpara() {
+
+        }
+        execute() {
+
+        }
+        undo() {
+
+        }
     }
-    
-    
-    public undo(){
-        var command = this._list.pop();
-        this.new_command = command;
-        this.new_command.undo();
+
+
+    export class CommandTileClick extends Command {
+        xtext: string;
+        ytext: string;
+        buttonColor: string;
+        buttonText: string;
+        TXsource: string;
+        TXwalkable: boolean;
+        TXnumber: number;
+        sucaiButtonText: string;
+        sucaiButtonColor: string;
+
+        private panelXt: render.TextField;
+        private panelYt: render.TextField;
+        private panelButton: ui.Button;
+        private panelTXsource: String[];
+        private panelTXwalkable: Boolean[];
+        private panelTXnumber: Number[];
+        private panelSucaiButton: ui.Button;
+        constructor(panelXt, panelYt, panelButton, panelTXsource, panelTXwalkable, panelTXnumber, panelSucaiButton) {
+            super();        
+            this.panelXt = panelXt;
+            this.panelYt = panelYt;
+            this.panelButton = panelButton;
+            this.panelTXsource = panelTXsource;
+            this.panelTXwalkable = panelTXwalkable;
+            this.panelTXnumber = panelTXnumber;
+            this.panelSucaiButton = panelSucaiButton;
+        }
+        getPara(xtext, ytext, buttonColor, buttonText, TXsource, TXwalkable, TXnumber, sucaiButtonText, sucaiButtonColor) {
+            this.xtext = xtext;
+            this.ytext = ytext;
+            this.buttonColor = buttonColor;
+            this.buttonText = buttonText;
+            this.TXsource = TXsource;
+            this.TXwalkable = TXwalkable;
+            this.TXnumber = TXnumber;
+            this.sucaiButtonText = sucaiButtonText;
+            this.sucaiButtonColor = sucaiButtonColor;
+        }
+
+        undo() {
+            this.panelXt.text = this.xtext;
+            this.panelYt.text = this.ytext;
+            this.panelButton.text = this.buttonText;
+            this.panelTXsource[0] = this.TXsource;
+            this.panelTXwalkable[0] = this.TXwalkable;
+            this.panelTXnumber[0] = this.TXnumber;
+            this.panelSucaiButton.text = this.sucaiButtonText;
+        }
 
     }
-    
-    public setCommand(command){
-        command.execute();
-        this._list.push(command);
+
+    export class CommandTXClick extends Command {
+        TXsource: string;
+        TXwalkable: boolean;
+        TXnumber: number;
+        sucaiButtonText: string;
+        sucaiButtonColor: string;
+
+        private panelTXsource: String[];
+        private panelTXwalkable: Boolean[];
+        private panelTXnumber: Number[];
+        private panelSucaiButton: ui.Button;
+        constructor(panelTXsource, panelTXwalkable, panelTXnumber, panelSucaiButton) {
+            super();
+            this.panelTXsource = panelTXsource;
+            this.panelTXwalkable = panelTXwalkable;
+            this.panelTXnumber = panelTXnumber;
+            this.panelSucaiButton = panelSucaiButton;
+        }
+        getPara(TXsource, TXwalkable, TXnumber, sucaiButtonText, sucaiButtonColor) {
+            this.TXsource = TXsource;
+            this.TXwalkable = TXwalkable;
+            this.TXnumber = TXnumber;
+            this.sucaiButtonText = sucaiButtonText;
+            this.sucaiButtonColor = sucaiButtonColor;
+        }
+        undo() {
+            this.panelTXsource[0] = this.TXsource;
+            this.panelTXwalkable[0] = this.TXwalkable;
+            this.panelTXnumber[0] = this.TXnumber;
+            this.panelSucaiButton.text = this.sucaiButtonText;
+        }
+
     }
-    
-}
+
+    export class CommandButtonClick extends Command {
+        x: number;
+        y: number;
+        TXdata: number;
+        MPdata: number;
+        buttonColor: string;
+        buttonText: string;
+        TXsource: string;
+        TXwalkable: boolean;
+        TXnumber: number;
+        sucaiButtonText: string;
+        sucaiButtonColor: string;
+        tileSource: string;
+        tileWalkable: number;
+
+        private panelButton: ui.Button;
+        private panelTXsource: String[];
+        private panelTXwalkable: Boolean[];
+        private panelTXnumber: Number[];
+        private panelSucaiButton: ui.Button;
+        private tile: editor.Tile;
+        constructor(panelButton, panelTXsource, panelTXwalkable, panelTXnumber, panelSucaiButton, tile) {
+            super();
+            this.panelButton = panelButton;
+            this.panelTXsource = panelTXsource;
+            this.panelTXwalkable = panelTXwalkable;
+            this.panelTXnumber = panelTXnumber;
+            this.panelSucaiButton = panelSucaiButton;
+            this.tile = tile;
+        }
+        getPara(x, y, TXdata, MPdata, buttonColor, buttonText, TXsource, TXwalkable, TXnumber, sucaiButtonText, sucaiButtonColor, tileSource, tileWalkable) {
+            this.x = x;
+            this.y = y;
+            this.TXdata = TXdata;
+            this.MPdata = MPdata;
+            this.buttonColor = buttonColor;
+            this.buttonText = buttonText;
+            this.TXsource = TXsource;
+            this.TXwalkable = TXwalkable;
+            this.TXnumber = TXnumber;
+            this.sucaiButtonText = sucaiButtonText;
+            this.sucaiButtonColor = sucaiButtonColor;
+            this.tileSource = tileSource;
+            this.tileWalkable = tileWalkable;
+        }
+        undo() {
+
+            mapData[this.y][this.x] = this.MPdata;
+            this.panelButton.text = this.buttonText;
+            this.panelTXsource[0] = this.TXsource;
+            this.panelTXwalkable[0] = this.TXwalkable;
+            this.panelTXnumber[0] = this.TXnumber;
+            this.panelSucaiButton.text = this.sucaiButtonText;
+            this.tile.source = this.tileSource;
+            this.tile.setWalkable(this.tileWalkable);
+        }
+    }
+
+    export class CommandSucaiButtonClick extends Command {
+        x: number;
+        y: number;
+        TXdata: number;
+        MPdata: number;
+        sucaiButtonColor: string;
+        tileSource: string;
+        tileSourceNum: number;
+
+        private panelSucaiButton: ui.Button;
+        private tile: editor.Tile;
+        constructor(panelSucaiButton, tile) {
+            super();
+            this.panelSucaiButton = panelSucaiButton;
+            this.tile = tile;
+        }
+        getPara(x, y, TXdata, MPdata, sucaiButtonColor, tileSource, tileSourceNum) {
+            this.x = x;
+            this.y = y;
+            this.TXdata = TXdata;
+            this.MPdata = MPdata;
+            this.sucaiButtonColor = sucaiButtonColor;
+            this.tileSource = tileSource;
+            this.tileSourceNum = tileSourceNum;
+        }
+        undo() {
+            mapData[this.y][this.x] = this.MPdata;
+            this.tile.source = this.tileSource;
+        }
+    }
 
 
-export class Command {   
-    execute(){
-       
-    }
-    
-}
-
-
-export class CommandA extends Command{
-    
-    public row;
-    public col;
-    public num;
-    public new_row;
-    public new_col;
-    public new_num;
-
-    constructor(row,col,num){
-        super();
-        this.row = row;
-        this.col = col;
-        this.num = num;
-                    
-    }
-      public undo(row,col,num){
-        console.log ("Undo " + this.row + this.col+ this.num);
-        this.getNewRow(this.row);
-        this.getNewCol(this.col);
-        this.getNewNum(this.num);       
-               
-    }  
- 
-    setNewRow(){
-        return this.new_row;
-    }
-    
-    getNewRow(row){
-        this.new_row = row;
-    }
-    
-    setNewCol(){
-        return this.new_col;
-    }
-    
-    getNewCol(col){
-        this.new_col = col;
-    }
-    
-    setNewNum(){
-        return this.new_num;
-    }
-    
-    getNewNum(num){
-        this.new_num = num;
-    }
-    public execute(){
-        console.log ("execute " + this.row + this.col);
-        
-    }
-    
-
-}
-    
 }
